b3ca3fda074fa3ad7ec6fe83f43428154b8668ba
quentintyr <quentin.tyr@gmail.com>
2025-08-24 19:59:54 +0200

    🔨REF | Update main mod initializer
    - Initialize NetworkManager on mod load
    - Add proper logging for mod initialization
    - Remove example code

--- src/main/java/dev/quentintyr/visiblearmorslots/Visiblearmorslots.java
+++ src/main/java/dev/quentintyr/visiblearmorslots/Visiblearmorslots.java
@@ -1,24 +1,30 @@
 package dev.quentintyr.visiblearmorslots;
 
+import dev.quentintyr.visiblearmorslots.network.NetworkManager;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Visiblearmorslots implements ModInitializer {
-	public static final String MOD_ID = "visiblearmorslots";
+    public static final String MOD_ID = "visiblearmorslots";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
-	}
+        LOGGER.info("Visible Armor Slots initializing...");
+
+        // Initialize network communication
+        NetworkManager.init();
+
+        LOGGER.info("Visible Armor Slots initialized!");
+    }
 }
